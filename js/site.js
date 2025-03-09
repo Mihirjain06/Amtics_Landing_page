e235b84b41bd84e42a248c09ab0084d5f3612606
Mihir Jain <148676259+Mihirjain06@users.noreply.github.com>
2025-03-09 09:31:39 +0530

    first commit

---
+++ js/site.js
@@ -0,0 +1,16 @@
+// JavaScript to include the header and footer
+document.addEventListener("DOMContentLoaded", function() {
+    // Load the header
+    fetch('html/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        });
+
+    // Load the footer
+    fetch('html/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        });
+});
